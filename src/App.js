adcc80cee7ea16884b6855556fea503f97cac4ce
unknown <dave8git@gmail.com>
2022-10-03 17:30:37 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState} from 'react';
+import Button from './components/Button/Button';
+import FormattedTime from './components/FortmattedTime/FormattedTime';
 
-function App() {
+const App = () => {
+
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(null);
+
+  const reset = () => {
+    setTime(0);
+    setTimer(null);
+  }
+
+  const start = () => {
+    setTimer(setInterval(() => {
+      setTimer(prevValue => prevValue + 0.01);
+    }, 10))
+  };
+
+  const stop = () => {
+    clearInterval(timer);
+    setTimer();
+  }
+
+  useEffect((timer) => {
+    return (timer) => {
+      if(timer) clearInterval(timer);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FormattedTime time={time}>{time}</FormattedTime>
+      <Button value={start}>START</Button>
+      <Button value={stop}>STOP</Button>
+      <Button value={reset}>RESET</Button>
     </div>
   );
-}
+};
 
-export default App;
+export default App; 
